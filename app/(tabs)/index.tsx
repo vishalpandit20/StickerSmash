9de37e3fe1c6680b149e10d50308f8e7ff125b4b
vishalpandit20 <vishalkumar88172@gmail.com>
2025-08-20 11:40:26 +0530

    completed the project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
-import { View, StyleSheet, ImageSourcePropType } from 'react-native';
+import { View, StyleSheet, ImageSourcePropType, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker'
 import { useRef, useState } from 'react';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef} from 'react-native-view-shot'
+import domtoimage from 'dom-to-image';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -56,6 +57,7 @@ export default function Index() {
   }
 
   const onSaveImageAsync = async() => {
+    if (Platform.OS !== 'web'){
     try{
       const localUri = await captureRef(imageRef, {
         height:440,
@@ -68,8 +70,23 @@ export default function Index() {
     } catch(e){
       console.log(e)
     }
-  }
+  }else {
+  try{
+    const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+      quality: 0.95,
+      width: 320,
+      height: 440,
+    })
 
+    let link = document.createElement('a');
+    link.download = 'sticker-smash.jpeg';
+    link.href = dataUrl;
+    link.click();
+  }catch (e){
+    console.log(e);
+  }
+}
+  }
 
 
   return (
